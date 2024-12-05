ecc8041abfc712f92253297c17a5862fb926b2f3
Victor Boleac <victor.boleac@icloud.com>
2024-12-05 09:43:04 +0000

    initial commit

---
+++ src/main/java/com/northcoders/recordshop/RecordshopApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/recordshop/RecordshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
