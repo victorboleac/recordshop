98737858466967c5d6290a1730d5092efd34a1dc
Victor Boleac <victor.boleac@icloud.com>
2024-12-05 18:57:45 +0000

    feat: added getAllAlbums endpoint to the AlbumController

--- src/main/java/com/northcoders/recordshop/controller/AlbumController.java
+++ src/main/java/com/northcoders/recordshop/controller/AlbumController.java
@@ -1,4 +1,27 @@
 package com.northcoders.recordshop.controller;
 
+import com.northcoders.recordshop.model.Album;
+import com.northcoders.recordshop.service.AlbumService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/albums")
 public class AlbumController {
+
+    private final AlbumService albumService;
+
+    public AlbumController(AlbumService albumService) {
+        this.albumService = albumService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Album>> getAllAlbums() {
+        List<Album> albums = albumService.getAllAlbums();
+        return ResponseEntity.ok(albums);
+    }
 }
