002c490908770e1fbe680680c2c064e5b8e9b520
Victor Boleac <victor.boleac@icloud.com>
2024-12-06 08:03:53 +0000

    test: checkArtistByName() returns false when artist name is null

--- src/test/java/com/northcoders/recordshop/service/ArtistServiceImplTest.java
+++ src/test/java/com/northcoders/recordshop/service/ArtistServiceImplTest.java
@@ -37,6 +37,17 @@ class ArtistServiceImplTest {
     }
 
     @Test
+    @DisplayName("checkArtistByName() returns false when artist name is null")
+    void testCheckArtistByName_NullInput() {
+        //Act
+        boolean result = artistServiceImpl.checkArtistByName(null);
+
+        // ASSERT
+        assertThat(result).isFalse();
+        verify(artistManagerRepository, never()).findByName(any());
+    }
+
+    @Test
     @DisplayName("checkArtistByName() returns false when artist does not exist")
     void testCheckArtistByName_ArtistDoesNotExist() {
         // Arrange
